71adb9fdba754c992b38862e32c5f9a78163dc9e
18176962906 <1761824166@qq.com>
2018-10-31 01:25:44 +0800

    update

---
+++ index.js
@@ -0,0 +1,32 @@
+//页面加载完成之后执行 
+window.onload=function(){
+//搜索区块的颜色变化
+search ()
+};
+//搜索区块的颜色变化
+function search(){
+//颜色随着页面的滚动逐渐加深
+//当我们超过轮播图的时候颜色保持不变
+//获取搜索盒子
+var searchBox=document.querySelector('.hm_header_box');
+//获取 banner盒子
+var bannerBox=document.querySelector('.hm_banner');
+//获取高度
+// var h=bannerBox.offsetHeight;
+//监听 window的滚动事件
+window. onscroll=function(){
+//不断的获取离顶部的距离
+var top=document. body.scrollTop;
+var opacity=0;
+if(top<h){
+//颜色随着页面的滚动逐渐加深
+opacity= top/h + 0.85
+}
+else{
+//当超过轮播图的时，颜色保持不变
+opacity=0.85;
+}
+//把透明度设置上去
+searchbox. style. background="raba(201, 21, 35,  "+opacity+")";
+}
+}
